04bc670bcb164dd0e5bb1c3026c940c866b39337
Jazzir Hussain <jasirhussain2002@gmail.com>
2022-11-03 18:59:52 +0530

    link sent in mail changd

--- pages/services/[name].jsx
+++ pages/services/[name].jsx
@@ -99,7 +99,7 @@ export default function Service({service}){
     var templateParams = {
       to_name: 'Manu',
       from_name:"GHC Growth Lab",
-      message: `A new appoinment has been made for ${service.post_title} by ${userName} on ${selecetdDate}-${currentMonthName}-${curentYear}. Please approve the request\n Link: https://ghc-growthlab.vercel.app/admin/appoinments` ,
+      message: `A new appoinment has been made for ${service.post_title} by ${userName} on ${selecetdDate}-${currentMonthName}-${curentYear}. Please approve the request\n Link: https://ghcgrowthlab.com/admin/appoinments` ,
       to_email: config.email
     };
     emailjs.send('service_fymwt6q','template_ut5nozq',templateParams,'KbGLQS0YcIzoUFKiy').then(()=>{
